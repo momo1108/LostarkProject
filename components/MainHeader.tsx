2605614ecc7e310ab21adcd7b7019727fb1bae19
momo1108 <momo1108@daum.net>
2023-04-27 17:59:15 +0900

    mainheader fix

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -8,7 +8,7 @@ export default function MainHeader(){
         setLoad(true);
     }, [])
 
-    return <div className={`${styles.container} ${load?styles.showUs:""}`}>
+    return <div className={`${styles.container}`}>
         <h1 className={`${styles.mainTitle} ${load?styles.showUs:""}`}>Loaple</h1>
         <h2 className={`${styles.subTitle} ${load?styles.showUs:""}`}> - Lostark Playground - </h2>
         <p className={`${styles.description} ${load?styles.showUs:""}`}>로스트아크를 씹고 뜯고 맛보고 즐기기 위한 간단한 웹 사이트입니다.</p>
