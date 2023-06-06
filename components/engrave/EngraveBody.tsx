3e881ddf0964ac993f994d7285dbd7b62c608bd5
momo1108 <momo1108@daum.net>
2023-06-06 18:19:53 +0900

    engrave start

--- components/engrave/EngraveBody.tsx
+++ components/engrave/EngraveBody.tsx
@@ -3,8 +3,9 @@ import { nanumNeo } from "@/types/GlobalType";
 
 export default function EngraveBody() {
   return (
-    <div className={`${styles.container} ${nanumNeo.className}`}>
-      Hello world
+    <div className={`${styles.body} ${nanumNeo.className}`}>
+      <EngraveSearchContainer />
+      <EngraveResultContainer />
     </div>
   );
 }
