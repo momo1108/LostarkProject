52caffb038e71016d51ffb0a54b0283810467751
momo1108 <momo1108@daum.net>
2023-06-06 12:37:12 +0900

    seo update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,21 +9,27 @@ export default function Document() {
           content="로스트아크,로아플,loaple,Loaple,로스트아크사이트"
         />
         <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Loaple" />
-        <meta property="og:title" content="Loaple - Lostark Helper" />
+        <meta property="og:site_name" content="로아플(Loaple)" />
+        <meta
+          property="og:title"
+          content="로아플(Loaple) - 로스트아크 도우미(Lostark Helper)"
+        />
         <meta
           property="og:description"
-          content="로아플은 세계에서 가장 작은 로스트아크 웹서비스로, 혼자 야무지게 만든 웹서비스입니다."
+          content="로스트아크 도구모음. 로아플은 세계에서 가장 작은 로스트아크 웹서비스로, 혼자 야무지게 만든 웹서비스입니다."
         />
         <meta property="og:image" content="/images/loaple_seo.jpg" />
         <meta property="og:url" content="http://loaple.site" />
 
         <meta property="twitter:card" content="summary" />
-        <meta property="twitter:site" content="Loaple" />
-        <meta property="twitter:title" content="Loaple - Lostark Helper" />
+        <meta property="twitter:site" content="로아플(Loaple)" />
+        <meta
+          property="twitter:title"
+          content="로아플(Loaple) - 로스트아크 도우미(Lostark Helper)"
+        />
         <meta
           property="twitter:description"
-          content="로아플은 세계에서 가장 작은 로스트아크 웹서비스로, 혼자 야무지게 만든 웹서비스입니다."
+          content="로스트아크 도구모음. 로아플은 세계에서 가장 작은 로스트아크 웹서비스로, 혼자 야무지게 만든 웹서비스입니다."
         />
         <meta property="twitter:image" content="/images/loaple_seo.jpg" />
         <meta property="twitter:url" content="http://loaple.site" />
