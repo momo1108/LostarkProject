0f5f87af12a49f60ac054a22e08aa12bf2673702
momo1108 <momo1108@daum.net>
2023-06-08 13:26:09 +0900

    seo update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,9 @@ export default function App({ Component, ...rest }: AppProps) {
   return (
     <Provider store={store}>
       <Head>
-        <title>로아플 - 로스트아크 유저를 위한 도구모음</title>
-        <title>Loaple - Lostark Helper</title>
+        <title>
+          로아플 - 로스트아크 유저를 위한 도구모음(Loaple - Lostark Helper)
+        </title>
       </Head>
       <Component
         className={`${nanumNeo.className} ${roboto.className}`}
