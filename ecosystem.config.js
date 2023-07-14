1116c69fd8d50d4fa237047ca2c9214eb45b511d
momo1108 <momo1108@daum.net>
2023-07-14 13:14:48 +0900

    pm2 ecosystem update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  apps : [{
-    name   : "buildstart",
-    script : "npm run build && npm start"
-  }, {
-    name   : "dev",
-    script : "npm run dev"
-  }]
-}
+  apps: [
+    {
+      name: "buildstart",
+      script: "npm run build && npm start",
+    },
+    {
+      name: "dev",
+      script: "npm run dev",
+    },
+    {
+      name: "start",
+      script: "npm run start",
+    },
+  ],
+};
