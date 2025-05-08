e80959aac38badd68d3978b5288628ebae8689e4
momo1108 <momo1108@daum.net>
2025-05-08 17:51:38 +0900

    chore: 린팅 및 포맷팅 관련 의존성 설치, 포맷팅 룰 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-    root: true,
-    extends: [
-      "next/core-web-vitals",
-      "plugin:prettier/recommended",
+  root: true,
+  extends: ["next/core-web-vitals", "plugin:prettier/recommended"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
     ],
-  };
+  },
+};
